fcdb9c871c1e3dd6e72c6b1036d2b8766f2523b7
Александр <alexandr.skobov@gmail.com>
2024-11-16 00:32:48 +0300

    Ðбазовые структуры данныхÐ

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1st program
+print(5 * 9 ** 0.5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 + 2 * 2)
+print( 2 * (2 + 2) )
+print( 2 + 2 * 2 == 2 * (2 + 2) )
+
+# 4th program
+s = '123.456'
+s = float(s)
+s = s * 10
+s = int(s)
+s = s % 10
+print(s)
