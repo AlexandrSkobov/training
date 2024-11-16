98d585b29ea10e9689e6f31b03493f1dd44a49c4
Александр <alexandr.skobov@gmail.com>
2024-11-17 01:33:08 +0300

    module_1_6

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+сourse_name = 'Python'
+print(f'Курс: {сourse_name}, всего задач: {number_of_completed_tasks},\
+ затрачено часов: {number_of_hours_spent}, среднее время выполнения: \
+{number_of_hours_spent / number_of_completed_tasks}')
