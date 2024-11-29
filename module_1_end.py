199f652e2edfdba4d155ba47915e872ee6581df2
Александр <alexandr.skobov@gmail.com>
2024-11-29 15:41:23 +0300

    module_1_end

--- module_1_end.py
+++ module_1_end.py
@@ -1,11 +1,11 @@
-my_dict = {}
+st_dict = {}
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 list_students = sorted(students)
 
 i = 0
 while i < len(list_students):
-    my_dict[list_students[i]] = sum(grades[i]) / len(grades[i])
+    st_dict[list_students[i]] = sum(grades[i]) / len(grades[i])
     i += 1
 
-print(my_dict)
+print(st_dict)
