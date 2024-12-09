72f4ff5a774d134e6c55a3a69f2ecfe4d0b060d1
Александр <alexandr.skobov@gmail.com>
2024-12-10 00:39:47 +0300

    call_a_func

--- call_a_func.py
+++ call_a_func.py
@@ -1,7 +1,6 @@
 import re
 
 def send_email(message, recipient, *, sender = 'university.help@gmail.com'):
-
     if sender == 'university.help@gmail.com':
         print()
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
@@ -20,5 +19,7 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
 
 
